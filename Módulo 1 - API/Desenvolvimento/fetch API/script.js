84411a4163af29acf141f6e10dbf0951fc5a0019
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-01-23 15:10:39 +0000

    feat: fetch api

---
+++ Módulo 1 - API/Desenvolvimento/fetch API/script.js
@@ -0,0 +1,20 @@
+// -------- Gerador de usuarios -------- //
+// 1. Captura de elementos
+const btnUsuario = document.getElementById('btn-usuario')
+const usuariosContainer = document.getElementById('usuarios-container')
+const helperTextUsuario = document.getElementById('helper-text-usuario')
+
+// 2. Funções
+function gerarUsuario(){
+
+}
+
+// 3. Eventos
+btnUsuario.addEventListener('click', gerarUsuario)
+
+// -------- Gerador de postagens -------- //
+// 1. Captura de elementos
+
+// 2. Funções
+
+// 3. Eventos
