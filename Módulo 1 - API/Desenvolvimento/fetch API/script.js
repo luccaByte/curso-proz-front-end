abd4251ae95c95a7447cebdb43808c36dca2e685
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-01-23 15:40:43 +0000

    feat: random data api (featch api)

--- Módulo 1 - API/Desenvolvimento/fetch API/script.js
+++ Módulo 1 - API/Desenvolvimento/fetch API/script.js
@@ -6,7 +6,25 @@ const helperTextUsuario = document.getElementById('helper-text-usuario')
 
 // 2. Funções
 function gerarUsuario(){
+    /*
+    const resposta = fetch('https://random-data-api.com/api/v2/users')
+    console.log(resposta)
 
+    const tratamentoResposta = resposta.then((res) => {
+        console.log(res)
+        return res.json() // é a mesma coisa que: JSON.parse(stringJSON)
+    })
+
+    tratamentoResposta.then((data) => {
+        console.log(data)
+    })
+    */
+
+    // outra maneira (mais simples kkk)
+
+    fetch('https://random-data-api.com/api/v2/users')
+        .then ((res) => res.json())
+        .then ((data) => console.log(data))
 }
 
 // 3. Eventos
