44bd0f28098f90daf7b6de23629769e24b9c02f2
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-02-05 15:54:49 +0000

    feat: export default

---
+++ Módulo 3 - npm/Desenvolvimento/import-export/js/calculadora.js
@@ -0,0 +1,19 @@
+const calculadora  = {
+    somar: (a, b) => a + b,
+    subtrair: (a, b) => a - b,
+    multiplicar: (a, b) => a * b,
+    dividir: (a, b) => a / b
+}
+
+export default calculadora;
+
+/* 
+outro modo, um modo mais direto de exportar
+
+export default {
+    somar: (a, b) => a + b,
+    subtrair: (a, b) => a - b,
+    multiplicar: (a, b) => a * b,
+    dividir: (a, b) => a / b
+}
+*/
--- Módulo 3 - npm/Desenvolvimento/import-export/js/script.js
+++ Módulo 3 - npm/Desenvolvimento/import-export/js/script.js
@@ -1,7 +1,19 @@
-import { somar, subtrair } from './operacoes.js'; // sempre colocar a extensão "operacoes.js"
+// import { somar, subtrair } from './operacoes.js'; // sempre colocar a extensão "operacoes.js"
 
-console.log('Hello');
 
-const result = somar(7, 4);
+// console.log('Hello');
 
-console.log(result);
+// const result = somar(7, 4);
+// let resultSub = subtrair (7, 4);
+
+// console.log(result);
+// console.log(resultSub);
+
+
+/* EXPORT DEFAULT */
+
+import modulo from "./calculadora.js"; // podemos renomear para qlqr nome(modulo (antes calculadora)), tomar cuidado na hora de usar o import pois tem que colocar o mesmo nome que atribuiu nesse import no resto das operações
+
+const resultado = modulo.multiplicar(4, 2);
+
+console.log(resultado);
