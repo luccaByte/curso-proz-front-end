04afc735d0281edc9b5389d0569b046734719c56
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-03-21 20:36:26 +0000

    feat: tipos de DOM (implicitos)

--- Módulo 4 - TypeScript/Tipagem Básica/script.ts
+++ Módulo 4 - TypeScript/Tipagem Básica/script.ts
@@ -1,8 +1,22 @@
-let numeroTs: number | string | boolean = 5; // tipagem explícita
-// podemos atribuir mais de um tipo para uma variável, usando pipe: |
+// let numeroTs: number | string | boolean = 5; // tipagem explícita
+// // podemos atribuir mais de um tipo para uma variável, usando pipe: |
 
-numeroTs = 10;
+// numeroTs = 10;
 
-numeroTs = 'Lucas';
+// numeroTs = 'Lucas';
 
-numeroTs = false;
+// numeroTs = false;
+
+let inputNome = document.querySelector('input');
+let titulo = document.querySelector('h1');
+let link = document.querySelector('a');
+
+if (inputNome) {
+    inputNome.value
+} else {
+    alert("nao foi possivel achar o elemento input")
+}
+
+// simplificando
+
+inputNome?.value
