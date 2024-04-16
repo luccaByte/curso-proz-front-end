9401d4c4933765d2e7eed63105ca9accee19d300
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-04-16 17:56:01 +0000

    feat: tipagem de objetos

--- Módulo 4 - TypeScript/Tipagem Intermediária/tipagem-intermediaria/src/main.ts
+++ Módulo 4 - TypeScript/Tipagem Intermediária/tipagem-intermediaria/src/main.ts
@@ -1,4 +1,51 @@
-import usuario from "./usuario";
+// para tipar o objeto é só colocar :
+// let usuario: {
+//     nome: string,
+//     idade: number,
+//     admin: boolean,
+//     endereco: {
+//         estado: string,
+//         cidade: string
+//     }
+// }
 
-console.log('olá, mundo!');
-console.log(usuario.nome);
+
+// para gerar varios usuarios apartir de um objeto, usar o type definindo o objeto com letra maiuscula
+type Usuario = {
+    nome: string,
+    idade: number,
+    admin: boolean,
+    endereco: {
+        estado: string,
+        cidade: string
+    }
+}
+
+// definindo
+let usuario: Usuario
+
+// para atribuir ao objeto usuario, usar =
+usuario = {
+    nome: "Lucas",
+    idade: 23,
+    admin: false,
+    endereco: {
+        estado: 'SP',
+        cidade: 'Caragua'
+    }
+}
+
+let usuario02: Usuario = {
+    nome: "Frieren",
+    idade: 2000,
+    admin: true,
+    endereco: {
+        estado: 'Seila',
+        cidade: 'N sei o que'
+    }
+}
+
+// tambem é possivel com numeros e etc
+type NumerosPermitidos = 2 | 3 | 5
+
+let numero: NumerosPermitidos = 5
--- Módulo 4 - TypeScript/Tipagem Intermediária/tipagem-intermediaria/src/usuario.ts
+++
@@ -1,3 +0,0 @@
-export default {
-    nome: 'lucas'
-}
