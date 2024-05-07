842b62baff719ee7e0eff892b7dcfc91bedf8de5
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-05-07 20:09:01 +0000

    operador ternário

---
+++ Módulo 5 - React/JavaScript para React/03_operador ternário/main.js
@@ -0,0 +1,14 @@
+// 3. operador ternário
+
+const idade = 23
+// let bebida 
+// if(idade >= 18){
+//     bebida = 'vinho'
+// } else {
+//     bebida = 'suco'
+// }
+
+// ternário
+let bebida = idade >= 18 ? 'vinho' : 'suco'
+
+console.log(bebida)
