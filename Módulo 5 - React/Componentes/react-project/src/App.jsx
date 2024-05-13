15615a29f9e5f23663afccea3b524593657c4965
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-05-13 16:07:12 +0000

    diferenças do jsx pro js

--- Módulo 5 - React/Componentes/react-project/src/App.jsx
+++ Módulo 5 - React/Componentes/react-project/src/App.jsx
@@ -1,5 +1,23 @@
+
 function App() {
-    return <h1>Arquivo app.jsx</h1>
+    const labelDoInput = 'idade'
+
+    return (
+        
+        <> {/* fragmentos: são tags vazias "<></>", são mais recomendaveis */}
+
+            <h1 className="titulo">Arquivo app.jsx</h1>
+            <p>texto do arquivo app.jsx</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero sunt explicabo distinctio alias voluptatibus labore adipisci dolor omnis, nobis recusandae, impedit eius fugiat necessitatibus placeat ducimus, provident voluptate sint sapiente.</p>
+
+            {/* podemos fechar qualquer tag nele mesmo, html não é permitido mas no react sim */}
+            <div />
+
+            <label htmlFor={labelDoInput}>Idade: </label>
+            <input type="text" id="idade" />
+
+        </>
+    )
 }
 
 export default App
