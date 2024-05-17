f0afa3aa2fd34ce6ab3db3c9da9456908d88d158
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-05-17 17:36:50 +0000

    renderização condicional II

--- Módulo 5 - React/Renderização/renderizacao/src/App.jsx
+++ Módulo 5 - React/Renderização/renderizacao/src/App.jsx
@@ -3,8 +3,8 @@ import Produto from "./components/Produto"
 function App(){
     return <>
         <h1>Renderização</h1>
-        <Produto nome='Guitarra' estoque={true}/>
-        <Produto nome='Ukulele' estoque={false}/>
+        <Produto nome='Guitarra' estoque={10}/>
+        <Produto nome='Ukulele' estoque={400}/>
         <Produto nome='Banjo' />
     </>
 }
--- Módulo 5 - React/Renderização/renderizacao/src/components/Produto.jsx
+++ Módulo 5 - React/Renderização/renderizacao/src/components/Produto.jsx
@@ -1,17 +1,15 @@
 import styles from '../styles/Produto.module.css'
 
 function Produto({nome, estoque}) {
-    let textoEstoque = estoque == true ? 'Disponível' : 'Indisponível' // if ternário
-    
-    // if (estoque == true) {
-    //     textoEstoque = 'Disponível'
-    // } else {
-    //     textoEstoque = 'Indisponível'
-    // }
+    let textoEstoque = estoque > 0 ? `${estoque} iten(s)` : 'Indisponível' // if ternário
 
-    return <div className={styles.container}>
+    // if (!estoque) return null
+
+    return <div className={`${styles.container} ${estoque ? '' : styles.indisponivel}`}>
         <h3>Produto: {nome}</h3>
-        <p>Estoque: {textoEstoque}</p>
+        <p style={ { color: estoque ? 'green' : 'red' } }>
+            Estoque: {textoEstoque}
+        </p>
     </div>
 }
 
